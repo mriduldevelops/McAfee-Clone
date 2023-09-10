7253baee676afe433dea02e44dd3228689c70b88
Mridul Sharma <sharmamridul045@gmail.com>
2023-09-10 14:00:49 +0530

    made site fully responsive

--- index.js
+++ index.js
@@ -1,32 +1,53 @@
-const accordians = document.querySelectorAll(".accordian");
-accordians.forEach((accordian)=>{
-    const symbol = accordian.querySelector(".symbol");
-    const desc = accordian.querySelector(".details");
+// accordians
 
-    accordian.addEventListener('click',()=>{
-        if (desc.classList.contains('hidden')){  
-            desc.classList.remove("hidden");          
-            symbol.textContent = "-";
-        }else{
-            desc.classList.add('hidden');
-            symbol.textContent = "+";
-        }
-    });
+const accordians = document.querySelectorAll(".accordian");
+accordians.forEach((accordian) => {
+  const symbol = accordian.querySelector(".symbol");
+  const desc = accordian.querySelector(".details");
+
+  accordian.addEventListener("click", () => {
+    if (desc.classList.contains("hidden")) {
+      desc.classList.remove("hidden");
+      symbol.textContent = "-";
+    } else {
+      desc.classList.add("hidden");
+      symbol.textContent = "+";
+    }
+  });
 });
+
+
+// faq accordians
 
 const faq = document.querySelectorAll(".faq__accordian");
 
-faq.forEach(acc =>{
-    const symbol = acc.querySelector('.symbol');
-    const desc = acc.querySelector('.details');
+faq.forEach((acc) => {
+  const symbol = acc.querySelector(".symbol");
+  const desc = acc.querySelector(".details");
 
-    acc.addEventListener('click', ()=>{
-        if (desc.classList.contains('hidden')){  
-            desc.classList.remove("hidden");          
-            symbol.classList.add('active')
-        }else{
-            desc.classList.add('hidden');
-            symbol.classList.remove('active')
-        }
-    })
-})
+  acc.addEventListener("click", () => {
+    if (desc.classList.contains("hidden")) {
+      desc.classList.remove("hidden");
+      symbol.classList.add("active");
+    } else {
+      desc.classList.add("hidden");
+      symbol.classList.remove("active");
+    }
+  });
+});
+
+
+// toggle button
+
+const toggleButton = document.querySelector(".hamburger_icon");
+const mobileNav = document.querySelector(".mobile__nav");
+
+toggleButton.addEventListener("click", () => {
+  if (toggleButton.classList.contains("not_active")) {
+    toggleButton.classList.remove("not_active");
+    mobileNav.classList.remove("hidden");
+  } else {
+    toggleButton.classList.add("not_active");
+    mobileNav.classList.add("hidden");
+  }
+});
